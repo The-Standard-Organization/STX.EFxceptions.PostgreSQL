2c5e0a6f61b65a15190aa46df6fe3f2e9cb7590a
Ricardo Cruz <rcruzfreelance@outlook.com>
2024-07-28 10:25:45 -0400

    INFRA: Initialize Unit Test Project

---
+++ STX.EFxceptions.PostgreSQL.Base.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,9 @@
+namespace STX.EFxceptions.PostgreSQL.Base.Tests.Unit
+{
+    public class DeletMe
+    {
+        [Fact]
+        public void ShouldBeTrue() =>
+            Assert.True(condition: true);
+    }
+}
