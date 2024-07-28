f88b166a5a878fb024f7fc8e7c566c06ce979a13
Ricardo Cruz <rcruzfreelance@outlook.com>
2024-07-28 13:08:57 -0400

    CODE RUB: Correted spelling and added header

--- STX.EFxceptions.PostgreSQL.Base.Tests.Unit/DeleteMe.cs
+++ STX.EFxceptions.PostgreSQL.Base.Tests.Unit/DeleteMe.cs
@@ -1,6 +1,10 @@
+// ----------------------------------------------------------------------------------
+// Copyright(c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
 namespace STX.EFxceptions.PostgreSQL.Base.Tests.Unit
 {
-    public class DeletMe
+    public class DeleteMe
     {
         [Fact]
         public void ShouldBeTrue() =>
