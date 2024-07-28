046b2096f8c2d38ae54969278c0f7726d1556b9e
Ricardo Cruz <rcruzfreelance@outlook.com>
2024-07-28 16:32:50 -0400

    INFRA: Initialize Build Project

---
+++ STX.EFxceptions.PostgreSQL.Infrastructure.Build/Program.cs
@@ -0,0 +1,17 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright(c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using STX.EFxceptions.PostgreSQL.Infrastructure.Build.Services;
+
+namespace STX.EFxceptions.PostgreSQL.Infrastructure.Build
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var scriptGenerationService = new ScriptGenerationService();
+            scriptGenerationService.GenerateBuildScript();
+        }
+    }
+}
