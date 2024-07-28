a2227bdc636bf4673548c001c4565ef13c183630
Ricardo Cruz <rcruzfreelance@outlook.com>
2024-07-28 17:31:54 -0400

    BROKERS: GetErrorCode

---
+++ STX.EFxceptions.PostgreSQL.Base/Brokers/DbErrorBroker/IPostgreSqlErrorBroker.cs
@@ -0,0 +1,12 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Npgsql;
+using STX.EFxceptions.Abstractions.Brokers.DbErrorBroker;
+
+namespace STX.EFxceptions.PostgreSQL.Base.Brokers.DbErrorBroker
+{
+    public interface IPostgreSqlErrorBroker : IDbErrorBroker<NpgsqlException>
+    { }
+}
---
+++ STX.EFxceptions.PostgreSQL.Base/Brokers/DbErrorBroker/PostgreSqlErrorBroker.cs
@@ -0,0 +1,14 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Npgsql;
+
+namespace STX.EFxceptions.PostgreSQL.Base.Brokers.DbErrorBroker
+{
+    public class PostgreSqlErrorBroker : IPostgreSqlErrorBroker
+    {
+        public int GetErrorCode(NpgsqlException postgreSqlException) =>
+             postgreSqlException.ErrorCode;
+    }
+}
