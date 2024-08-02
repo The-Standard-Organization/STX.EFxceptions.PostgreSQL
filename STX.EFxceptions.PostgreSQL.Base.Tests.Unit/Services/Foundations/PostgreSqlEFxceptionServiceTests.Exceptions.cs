4be2e4e5e7291484c8faf608082252a57c76a8a1
Ricardo Cruz <rcruzfreelance@outlook.com>
2024-08-02 18:29:50 -0400

    ShouldThrowDbUpdateExceptionIfErrorCodeIsNotRecognized -> FAIL

---
+++ STX.EFxceptions.PostgreSQL.Base.Tests.Unit/Services/Foundations/PostgreSqlEFxceptionServiceTests.Exceptions.cs
@@ -0,0 +1,60 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Npgsql;
+
+namespace STX.EFxceptions.PostgreSQL.Base.Tests.Unit.Services.Foundations
+{
+    public partial class PostgreSqlEFxceptionServiceTests
+    {
+        [Fact]
+        public void ShouldThrowDbUpdateExceptionIfErrorCodeIsNotRecognized()
+        {
+            // given
+            int sqlForeignKeyConstraintConflictErrorCode = 0000;
+            string randomErrorMessage = CreateRandomErrorMessage();
+
+            NpgsqlException foreignKeyConstraintConflictExceptionThrown =
+                CreatePostgreSqlException(
+                    message: randomErrorMessage,
+                    errorCode: sqlForeignKeyConstraintConflictErrorCode);
+
+            var dbUpdateException = new DbUpdateException(
+                message: randomErrorMessage,
+                innerException: foreignKeyConstraintConflictExceptionThrown);
+
+            DbUpdateException expectedDbUpdateException = dbUpdateException;
+
+            this.postgreSqlErrorBrokerMock.Setup(broker =>
+                broker.GetErrorCode(foreignKeyConstraintConflictExceptionThrown))
+                    .Returns(sqlForeignKeyConstraintConflictErrorCode);
+
+            // when
+            DbUpdateException actualDbUpdateException =
+                Assert.Throws<DbUpdateException>(() => this.postgreSqlEFxceptionService
+                    .ThrowMeaningfulException(dbUpdateException));
+
+            // then
+            actualDbUpdateException.Should()
+                .BeEquivalentTo(
+                expectation: expectedDbUpdateException,
+                config: options => options
+                    .Excluding(ex => ex.TargetSite)
+                    .Excluding(ex => ex.StackTrace)
+                    .Excluding(ex => ex.Source)
+                    .Excluding(ex => ex.InnerException.TargetSite)
+                    .Excluding(ex => ex.InnerException.StackTrace)
+                    .Excluding(ex => ex.InnerException.Source));
+
+            this.postgreSqlErrorBrokerMock.Verify(broker => broker
+                .GetErrorCode(foreignKeyConstraintConflictExceptionThrown), Times.Once());
+
+            postgreSqlErrorBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
