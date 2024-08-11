581a19474d443f4b740a0a4a8238ac1e0deae177
Ricardo Cruz <rcruzfreelance@outlook.com>
2024-08-11 14:13:28 -0400

    CODE RUB: Use SqlState instead of ErrorCode

--- STX.EFxceptions.PostgreSQL.Base/Brokers/DbErrorBroker/PostgreSqlErrorBroker.cs
+++ STX.EFxceptions.PostgreSQL.Base/Brokers/DbErrorBroker/PostgreSqlErrorBroker.cs
@@ -9,6 +9,6 @@ namespace STX.EFxceptions.PostgreSQL.Base.Brokers.DbErrorBroker
     public class PostgreSqlErrorBroker : IPostgreSqlErrorBroker
     {
         public string GetErrorCode(NpgsqlException postgreSqlException) =>
-             postgreSqlException.ErrorCode.ToString();
+             postgreSqlException.SqlState;
     }
 }
