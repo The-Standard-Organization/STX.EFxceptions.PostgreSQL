c0a59287411919eb877f04052b75cf6c84aa3dd7
Ricardo Cruz <rcruzfreelance@outlook.com>
2025-05-25 15:05:32 -0400

    EXPOSER: Postgre SQL Identity Db Context

---
+++ STX.EFxceptions.Identity.PostgreSQL/EFxceptionsIdentityContext.cs.cs
@@ -0,0 +1,69 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright(c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+
+using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using STX.EFxceptions.Abstractions.Brokers.DbErrorBroker;
+using STX.EFxceptions.Abstractions.Services.EFxceptions;
+using STX.EFxceptions.Identity.Core;
+using STX.EFxceptions.PostgreSQL.Base.Brokers.DbErrorBroker;
+using STX.EFxceptions.PostgreSQL.Base.Services.Foundations;
+
+namespace STX.EFxceptions.Identity.PostgreSQL
+{
+    public abstract class EFxceptionsIdentityContext<TUser, TRole, TKey>
+         : IdentityDbContextBase<TUser, TRole, TKey, IdentityUserClaim<TKey>, IdentityUserRole<TKey>,
+             IdentityUserLogin<TKey>, IdentityRoleClaim<TKey>, IdentityUserToken<TKey>, NpgsqlException, string>
+        where TUser : IdentityUser<TKey>
+        where TRole : IdentityRole<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        public EFxceptionsIdentityContext(DbContextOptions options) : base(options)
+        { }
+
+        protected EFxceptionsIdentityContext() : base()
+        { }
+
+        protected override IDbErrorBroker<NpgsqlException, string> CreateErrorBroker() =>
+            new PostgreSqlErrorBroker();
+
+        protected override IEFxceptionService CreateEFxceptionService(
+            IDbErrorBroker<NpgsqlException, string> errorBroker)
+        {
+            return new PostgreSqlEFxceptionService(errorBroker);
+        }
+    }
+
+    public abstract class EFxceptionsIdentityContext<
+        TUser, TRole, TKey, TUserClaim, TUserRole, TUserLogin, TRoleClaim, TUserToken>
+        : IdentityDbContextBase<TUser, TRole, TKey,
+            TUserClaim, TUserRole, TUserLogin, TRoleClaim, TUserToken, NpgsqlException, string>
+        where TUser : IdentityUser<TKey>
+        where TRole : IdentityRole<TKey>
+        where TKey : IEquatable<TKey>
+        where TUserClaim : IdentityUserClaim<TKey>
+        where TUserRole : IdentityUserRole<TKey>
+        where TUserLogin : IdentityUserLogin<TKey>
+        where TRoleClaim : IdentityRoleClaim<TKey>
+        where TUserToken : IdentityUserToken<TKey>
+    {
+        public EFxceptionsIdentityContext(DbContextOptions options) : base(options)
+        { }
+
+        protected EFxceptionsIdentityContext() : base()
+        { }
+
+        protected override IDbErrorBroker<NpgsqlException, string> CreateErrorBroker() =>
+            new PostgreSqlErrorBroker();
+
+        protected override IEFxceptionService CreateEFxceptionService(
+            IDbErrorBroker<NpgsqlException, string> errorBroker)
+        {
+            return new PostgreSqlEFxceptionService(errorBroker);
+        }
+    }
+}
